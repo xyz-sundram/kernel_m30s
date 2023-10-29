953dbe696e8fbe9a7cec519d09cf5199115aaccc
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-10-29 12:22:08 +0000

    Revert "remove if CONFIG_HAVE_KSU_WITHOUT_KPROBES"
    This reverts commit 22959799b06c7ae415f9145640b7555404c8deb8.

--- fs/exec.c
+++ fs/exec.c
@@ -1929,11 +1929,13 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_HAVE_KSU_WITHOUT_KPROBES
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 		void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 		void *argv, void *envp, int *flags);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
@@ -1946,10 +1948,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_HAVE_KSU_WITHOUT_KPROBES
 	if (unlikely(ksu_execveat_hook))
 	    ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 	    ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
